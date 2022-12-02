eecd5100e8364971fe09407cf574b9f7fb900e34
Mirzakhmet Syzdykov <mspmail598@gmail.com>
2022-12-02 22:03:20 +0600

    Edited

--- mollymage/src/main/java/com/codenjoy/dojo/games/mollymage/YourSolver.java
+++ mollymage/src/main/java/com/codenjoy/dojo/games/mollymage/YourSolver.java
@@ -99,7 +99,7 @@ public class YourSolver implements Solver<Board> {
     }
 
     public boolean goodMove(int x, int y) {
-        for (int i = 1; i <= 6; ++i) { // phony
+        for (int i = 1; i <= 6; ++i) {
             if (isBlastPoint(x + i, y) ) return false;
             
 			if (isBlastPoint(x - i, y) ) return false;
@@ -159,302 +159,302 @@ public class YourSolver implements Solver<Board> {
         return true;
     }
 
-    public String getNextCommand() {
-        int hx = this.board.getHero().getX();
+	public String getNextCommand() {
+		int hx = this.board.getHero().getX();
 
-        int hy = this.board.getHero().getY();
+		int hy = this.board.getHero().getY();
 
-        HashSet<String> hsDiag = new HashSet<>();
+		HashSet<String> hsDiag = new HashSet<>();
 
-        if (this.board.getAt(hx - 1, hy) == Element.NONE) {
-            if (this.board.getAt(hx - 1, hy - 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_LEFT);
-            }
+		if (this.board.getAt(hx - 1, hy) == Element.NONE) {
+			if (this.board.getAt(hx - 1, hy - 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_LEFT);
+			}
 
-            if (this.board.getAt(hx - 1, hy + 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_LEFT);
-            }
-        }
+			if (this.board.getAt(hx - 1, hy + 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_LEFT);
+			}
+		}
 
-        if (this.board.getAt(hx + 1, hy) == Element.NONE) {
-            if (this.board.getAt(hx + 1, hy - 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_RIGHT);
-            }
+		if (this.board.getAt(hx + 1, hy) == Element.NONE) {
+			if (this.board.getAt(hx + 1, hy - 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_RIGHT);
+			}
 
-            if (this.board.getAt(hx + 1, hy + 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_RIGHT);
-            }
-        }
+			if (this.board.getAt(hx + 1, hy + 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_RIGHT);
+			}
+		}
 
-        if (this.board.getAt(hx, hy - 1) == Element.NONE) {
-            if (this.board.getAt(hx - 1, hy - 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_DOWN);
-            }
+		if (this.board.getAt(hx, hy - 1) == Element.NONE) {
+			if (this.board.getAt(hx - 1, hy - 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_DOWN);
+			}
 
-            if (this.board.getAt(hx + 1, hy - 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_DOWN);
-            }
-        }
+			if (this.board.getAt(hx + 1, hy - 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_DOWN);
+			}
+		}
 
-        if (this.board.getAt(hx, hy + 1) == Element.NONE) {
-            if (this.board.getAt(hx - 1, hy + 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_UP);
-            }
+		if (this.board.getAt(hx, hy + 1) == Element.NONE) {
+			if (this.board.getAt(hx - 1, hy + 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_UP);
+			}
 
-            if (this.board.getAt(hx + 1, hy + 1) == Element.NONE) {
-                hsDiag.add(Command.MOVE_UP);
-            }
-        }
+			if (this.board.getAt(hx + 1, hy + 1) == Element.NONE) {
+				hsDiag.add(Command.MOVE_UP);
+			}
+		}
 
-        if (hsDiag.size() > 0) {
-            return hsDiag.toArray()[random.nextInt(hsDiag.size())].toString();
-        }
+		if (hsDiag.size() > 0) {
+			return hsDiag.toArray()[random.nextInt(hsDiag.size())].toString();
+		}
 
-        boolean bfLeft = true;
-        boolean bfRight = true;
-        boolean bfUp = true;
-        boolean bfDown = true;
+		boolean bfLeft = true;
+		boolean bfRight = true;
+		boolean bfUp = true;
+		boolean bfDown = true;
 
-        for (int i = 1; i <= 4; ++i) { // phony
-            if (this.board.getAt(hx - i, hy) != Element.NONE) {
-                bfLeft = false;
-            }
+		for (int i = 1; i <= 4; ++i) {
+			if (this.board.getAt(hx - i, hy) != Element.NONE) {
+				bfLeft = false;
+			}
 
-            if (this.board.getAt(hx + i, hy) != Element.NONE) {
-                bfRight = false;
-            }
+			if (this.board.getAt(hx + i, hy) != Element.NONE) {
+				bfRight = false;
+			}
 
-            if (this.board.getAt(hx, hy + i) != Element.NONE) {
-                bfUp = false;
-            }
+			if (this.board.getAt(hx, hy + i) != Element.NONE) {
+				bfUp = false;
+			}
 
-            if (this.board.getAt(hx, hy - i) != Element.NONE) {
-                bfDown = false;
-            }
-        }
+			if (this.board.getAt(hx, hy - i) != Element.NONE) {
+				bfDown = false;
+			}
+		}
 
-        HashSet<String> hs = new HashSet<>();
+		HashSet<String> hs = new HashSet<>();
 
-        if (bfLeft) {
-            hs.add(Command.MOVE_LEFT);
-        }
+		if (bfLeft) {
+			hs.add(Command.MOVE_LEFT);
+		}
 
-        if (bfRight) {
-            hs.add(Command.MOVE_RIGHT);
-        }
+		if (bfRight) {
+			hs.add(Command.MOVE_RIGHT);
+		}
 
-        if (bfUp) {
-            hs.add(Command.MOVE_UP);
-        }
+		if (bfUp) {
+			hs.add(Command.MOVE_UP);
+		}
 
-        if (bfDown) {
-            hs.add(Command.MOVE_DOWN);
-        }
+		if (bfDown) {
+			hs.add(Command.MOVE_DOWN);
+		}
 
-        if (hs.size() > 0) {
-            return hs.toArray()[random.nextInt(hs.size())].toString();
-        }
+		if (hs.size() > 0) {
+			return hs.toArray()[random.nextInt(hs.size())].toString();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
     public String prevMoveCommand = null;
 
     public int prevCommandCounter = -1;
 
-    @Override
-    public String get(Board board) {
-        this.board = board;
-        if (board.isGameOver()) return "";
+	@Override
+	public String get(Board board) {
+		this.board = board;
+		if (board.isGameOver()) return "";
 
-        if (this.nextCommand != null) {
+		if (this.nextCommand != null) {
 
-            prevCommand = this.nextCommand;
+			prevCommand = this.nextCommand;
 
-            this.nextCommand = null;
+			this.nextCommand = null;
 
-            return prevCommand;
-        }
+			return prevCommand;
+		}
 
 
-        HashSet<String> avails = new HashSet<>();
+		HashSet<String> avails = new HashSet<>();
 
-        HashSet<String> avails1 = new HashSet<>();
+		HashSet<String> avails1 = new HashSet<>();
 
-        HashSet<String> avails2 = new HashSet<>();
+		HashSet<String> avails2 = new HashSet<>();
 
-        if (goodCell(this.board.getHero().getX() - 1, this.board.getHero().getY()) //&& board.getAt(this.board.getHero().getX() - 1, this.board.getHero().getY()) == Element.NONE
-            && board.getAt(this.board.getHero().getX() - 1, this.board.getHero().getY()) != Element.GHOST
-                && goodMove(this.board.getHero().getX() - 1, this.board.getHero().getY())
-                && board.getAt(this.board.getHero().getX() - 1, this.board.getHero().getY()) != Element.TREASURE_BOX) {
-            avails.add(Command.MOVE_LEFT);
+		if (goodCell(this.board.getHero().getX() - 1, this.board.getHero().getY())
+			&& board.getAt(this.board.getHero().getX() - 1, this.board.getHero().getY()) != Element.GHOST
+				&& goodMove(this.board.getHero().getX() - 1, this.board.getHero().getY())
+				&& board.getAt(this.board.getHero().getX() - 1, this.board.getHero().getY()) != Element.TREASURE_BOX) {
+			avails.add(Command.MOVE_LEFT);
 
-            avails1.add(Command.MOVE_LEFT);
-        }
+			avails1.add(Command.MOVE_LEFT);
+		}
 
-        if (goodCell(this.board.getHero().getX() + 1, this.board.getHero().getY())//board.getAt(this.board.getHero().getX() + 1, this.board.getHero().getY()) == Element.NONE
-                && board.getAt(this.board.getHero().getX() + 1, this.board.getHero().getY()) != Element.GHOST
-                && goodMove(this.board.getHero().getX() + 1, this.board.getHero().getY())
-                && board.getAt(this.board.getHero().getX() + 1, this.board.getHero().getY()) != Element.TREASURE_BOX) {
-            avails.add(Command.MOVE_RIGHT);
+		if (goodCell(this.board.getHero().getX() + 1, this.board.getHero().getY())
+				&& board.getAt(this.board.getHero().getX() + 1, this.board.getHero().getY()) != Element.GHOST
+				&& goodMove(this.board.getHero().getX() + 1, this.board.getHero().getY())
+				&& board.getAt(this.board.getHero().getX() + 1, this.board.getHero().getY()) != Element.TREASURE_BOX) {
+			avails.add(Command.MOVE_RIGHT);
 
-            avails1.add(Command.MOVE_RIGHT);
-        }
+			avails1.add(Command.MOVE_RIGHT);
+		}
 
-        if (goodCell(this.board.getHero().getX(), this.board.getHero().getY() - 1)//board.getAt(this.board.getHero().getX(), this.board.getHero().getY() - 1) == Element.NONE
-                && board.getAt(this.board.getHero().getX(), this.board.getHero().getY() - 1) != Element.GHOST
-                && goodMove(this.board.getHero().getX(), this.board.getHero().getY() - 1)
-                && board.getAt(this.board.getHero().getX(), this.board.getHero().getY() - 1) != Element.TREASURE_BOX) {
-            avails.add(Command.MOVE_DOWN);
+		if (goodCell(this.board.getHero().getX(), this.board.getHero().getY() - 1)
+				&& board.getAt(this.board.getHero().getX(), this.board.getHero().getY() - 1) != Element.GHOST
+				&& goodMove(this.board.getHero().getX(), this.board.getHero().getY() - 1)
+				&& board.getAt(this.board.getHero().getX(), this.board.getHero().getY() - 1) != Element.TREASURE_BOX) {
+			avails.add(Command.MOVE_DOWN);
 
-            avails2.add(Command.MOVE_DOWN);
-        }
+			avails2.add(Command.MOVE_DOWN);
+		}
 
-        if (goodCell(this.board.getHero().getX(), this.board.getHero().getY() + 1) //board.getAt(this.board.getHero().getX(), this.board.getHero().getY() + 1) == Element.NONE
-                && board.getAt(this.board.getHero().getX(), this.board.getHero().getY() + 1) != Element.GHOST
-                && goodMove(this.board.getHero().getX(), this.board.getHero().getY() + 1)
-                && board.getAt(this.board.getHero().getX(), this.board.getHero().getY() + 1) != Element.TREASURE_BOX) {
-            avails.add(Command.MOVE_UP);
+		if (goodCell(this.board.getHero().getX(), this.board.getHero().getY() + 1)
+				&& board.getAt(this.board.getHero().getX(), this.board.getHero().getY() + 1) != Element.GHOST
+				&& goodMove(this.board.getHero().getX(), this.board.getHero().getY() + 1)
+				&& board.getAt(this.board.getHero().getX(), this.board.getHero().getY() + 1) != Element.TREASURE_BOX) {
+			avails.add(Command.MOVE_UP);
 
-            avails2.add(Command.MOVE_UP);
-        }
+			avails2.add(Command.MOVE_UP);
+		}
 
-        String t = this.getNextCommand();
+		String t = this.getNextCommand();
 
-        if ((avails1.size() > 0 || avails2.size() > 0) && t != null && noDamagePotion() /*&& random.nextInt(100) <= 30*/ && numberOfPotions > 0 && potionStepsRemained == 0) {
-            potionX = board.getHero().getX();
+		if ((avails1.size() > 0 || avails2.size() > 0) && t != null && noDamagePotion() && numberOfPotions > 0 && potionStepsRemained == 0) {
+			potionX = board.getHero().getX();
 
-            potionY = board.getHero().getY();
+			potionY = board.getHero().getY();
 
-            potionStepsRemained = 6;
+			potionStepsRemained = 6;
 
-            this.nextCommand = t;
+			this.nextCommand = t;
 
-            return Command.DROP_POTION;
-        } else if (potionStepsRemained > 0) {
-            --potionStepsRemained;
+			return Command.DROP_POTION;
+		} else if (potionStepsRemained > 0) {
+			--potionStepsRemained;
 
-            if ((potionX != this.board.getHero().getX()) && (potionY != this.board.getHero().getY())) {
-                return Command.NONE;
-            }
+			if ((potionX != this.board.getHero().getX()) && (potionY != this.board.getHero().getY())) {
+				return Command.NONE;
+			}
 
-            for (String s: avails) {
-                if (s.equals(Command.MOVE_DOWN)) {
-                    if (potionY - 1 != this.board.getHero().getY() && potionX != this.board.getHero().getX()) {
-                        return prevCommand = s;
-                    }
-                } else if (s.equals(Command.MOVE_UP)) {
-                    if (potionY + 1 != this.board.getHero().getY() && potionX != this.board.getHero().getX()) {
-                        return prevCommand = s;
-                    }
-                } else if (s.equals(Command.MOVE_LEFT)) {
-                    if (potionX - 1 != this.board.getHero().getX() && potionY != this.board.getHero().getY()) {
-                        return prevCommand = s;
-                    }
-                } else if (s.equals(Command.MOVE_RIGHT)) {
-                    if (potionX + 1 != this.board.getHero().getX() && potionY != this.board.getHero().getY()) {
-                        return prevCommand = s;
-                    }
-                }
-            }
+			for (String s: avails) {
+				if (s.equals(Command.MOVE_DOWN)) {
+					if (potionY - 1 != this.board.getHero().getY() && potionX != this.board.getHero().getX()) {
+						return prevCommand = s;
+					}
+				} else if (s.equals(Command.MOVE_UP)) {
+					if (potionY + 1 != this.board.getHero().getY() && potionX != this.board.getHero().getX()) {
+						return prevCommand = s;
+					}
+				} else if (s.equals(Command.MOVE_LEFT)) {
+					if (potionX - 1 != this.board.getHero().getX() && potionY != this.board.getHero().getY()) {
+						return prevCommand = s;
+					}
+				} else if (s.equals(Command.MOVE_RIGHT)) {
+					if (potionX + 1 != this.board.getHero().getX() && potionY != this.board.getHero().getY()) {
+						return prevCommand = s;
+					}
+				}
+			}
 
-            if (potionY == this.board.getHero().getY()) {
-                if (this.board.getHero().getX() > potionX && avails1.contains(Command.MOVE_RIGHT)) {
-                    return prevCommand = Command.MOVE_RIGHT;
-                } else if (avails1.contains(Command.MOVE_LEFT)) {
-                    return prevCommand = Command.MOVE_LEFT;
-                }
+			if (potionY == this.board.getHero().getY()) {
+				if (this.board.getHero().getX() > potionX && avails1.contains(Command.MOVE_RIGHT)) {
+					return prevCommand = Command.MOVE_RIGHT;
+				} else if (avails1.contains(Command.MOVE_LEFT)) {
+					return prevCommand = Command.MOVE_LEFT;
+				}
 
-                return Command.NONE;
-            }
+				return Command.NONE;
+			}
 
-            if (potionX == this.board.getHero().getX()) {
-                if (this.board.getHero().getY() > potionY && avails2.contains(Command.MOVE_UP)) {
-                    return prevCommand = Command.MOVE_UP;
-                } else if (avails2.contains(Command.MOVE_DOWN)) {
-                    return prevCommand = Command.MOVE_DOWN;
-                }
+			if (potionX == this.board.getHero().getX()) {
+				if (this.board.getHero().getY() > potionY && avails2.contains(Command.MOVE_UP)) {
+					return prevCommand = Command.MOVE_UP;
+				} else if (avails2.contains(Command.MOVE_DOWN)) {
+					return prevCommand = Command.MOVE_DOWN;
+				}
 
-                return Command.NONE;
-            }
+				return Command.NONE;
+			}
 
-        if (avails.size() == 0) {
-            return Command.NONE;
-        }
+		if (avails.size() == 0) {
+			return Command.NONE;
+		}
 
-        Map<Integer, String> ad = new HashMap<>();
+		Map<Integer, String> ad = new HashMap<>();
 
-        for (String s: avails) {
-            List<Point> ghosts = new ArrayList<>();
-            List<Point> potions = this.board.getPotions();
+		for (String s: avails) {
+			List<Point> ghosts = new ArrayList<>();
+			List<Point> potions = this.board.getPotions();
 
-            ghosts.addAll(potions);
+			ghosts.addAll(potions);
 
-            int mean = -1;
+			int mean = -1;
 
-            if (s.equals(Command.MOVE_LEFT)) {
-                for (Point pt : ghosts) {
-                    int k = Math.abs(this.board.getHero().getX() - 1 - pt.getX())
-                            + Math.abs(this.board.getHero().getY() - pt.getY());
+			if (s.equals(Command.MOVE_LEFT)) {
+				for (Point pt : ghosts) {
+					int k = Math.abs(this.board.getHero().getX() - 1 - pt.getX())
+							+ Math.abs(this.board.getHero().getY() - pt.getY());
 
-                    if (k < mean || mean == -1) mean = k;
-                }
+					if (k < mean || mean == -1) mean = k;
+				}
 
-                if (mean != -1) ad.put(mean, s);
-            }
+				if (mean != -1) ad.put(mean, s);
+			}
 
-            mean = -1;
+			mean = -1;
 
-            if (s.equals(Command.MOVE_RIGHT)) {
-                for (Point pt : ghosts) {
-                    int k = Math.abs(this.board.getHero().getX() + 1 - pt.getX())
-                            + Math.abs(this.board.getHero().getY() - pt.getY());
+			if (s.equals(Command.MOVE_RIGHT)) {
+				for (Point pt : ghosts) {
+					int k = Math.abs(this.board.getHero().getX() + 1 - pt.getX())
+							+ Math.abs(this.board.getHero().getY() - pt.getY());
 
-                    if (k < mean || mean == -1) mean = k;
-                }
+					if (k < mean || mean == -1) mean = k;
+				}
 
-                if (mean != -1) ad.put(mean, s);
-            }
+				if (mean != -1) ad.put(mean, s);
+			}
 
-            mean = -1;
+			mean = -1;
 
-            if (s.equals(Command.MOVE_UP)) {
-                for (Point pt : ghosts) {
-                    int k = Math.abs(this.board.getHero().getX() - pt.getX())
-                            + Math.abs(this.board.getHero().getY() + 1 - pt.getY());
+			if (s.equals(Command.MOVE_UP)) {
+				for (Point pt : ghosts) {
+					int k = Math.abs(this.board.getHero().getX() - pt.getX())
+							+ Math.abs(this.board.getHero().getY() + 1 - pt.getY());
 
-                    if (k < mean || mean == -1) mean = k;
-                }
+					if (k < mean || mean == -1) mean = k;
+				}
 
-                if (mean != -1) ad.put(mean, s);
-            }
+				if (mean != -1) ad.put(mean, s);
+			}
 
-            mean = -1;
+			mean = -1;
 
-            if (s.equals(Command.MOVE_DOWN)) {
-                for (Point pt : ghosts) {
-                    int k = Math.abs(this.board.getHero().getX() - pt.getX())
-                            + Math.abs(this.board.getHero().getY() - 1 - pt.getY());
+			if (s.equals(Command.MOVE_DOWN)) {
+				for (Point pt : ghosts) {
+					int k = Math.abs(this.board.getHero().getX() - pt.getX())
+							+ Math.abs(this.board.getHero().getY() - 1 - pt.getY());
 
-                    if (k < mean || mean == -1) mean = k;
-                }
+					if (k < mean || mean == -1) mean = k;
+				}
 
-                if (mean != -1) ad.put(mean, s);
-            }
-        }
+				if (mean != -1) ad.put(mean, s);
+			}
+		}
 
-        int vmax = -1;
+		int vmax = -1;
 
-        for (Integer ii: ad.keySet()) {
-            if (ii > vmax) {
-                vmax = ii;
-            }
-        }
+		for (Integer ii: ad.keySet()) {
+			if (ii > vmax) {
+				vmax = ii;
+			}
+		}
 
-        if (vmax != -1 && vmax > 0 && vmax < 4) {
-            return Command.NONE;
-        }
+		if (vmax != -1 && vmax > 0 && vmax < 4) {
+			return Command.NONE;
+		}
 
-        return avails.toArray()[random.nextInt(avails.size())].toString();
-    }
+		return avails.toArray()[random.nextInt(avails.size())].toString();
+	}
 }
